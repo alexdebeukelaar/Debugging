7fdd5bc04d4784317214be8e9bdead01b9d12335
Alex de Beukelaar <82021515+alexdebeukelaar@users.noreply.github.com>
2021-05-20 15:13:30 +0200

    exercise 4&5

--- 2.debugging/resources/expert.php
+++ 2.debugging/resources/expert.php
@@ -23,3 +23,24 @@ $monday = $week[0];
 echo $monday;
 
 //An array begins at 0.
+
+new_exercise(3);
+// === Exercise 3 ===
+// This should echo ` "Debugged !" `, fix it so that that is the literal text echo'ed
+
+$str = 'Debugged ! Also very fun';
+echo substr($str, 0, 10);
+
+// ' instead of "
+
+new_exercise(4);
+// === Exercise 4 ===
+// Sometimes debugging code is just like looking up code and syntax...
+// The print_r($week) should give:  Array ( [0] => mon [1] => tues [2] => wednes [3] => thurs [4] => fri [5] => satur [6] => sun )
+// Look up whats going wrong with this code, and then fix it, with ONE CHARACTER!
+
+foreach($week as &$day) {
+    $day = substr($day, 0, strlen($day)-3);
+}
+
+print_r($week);
